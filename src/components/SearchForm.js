96dac4e0f699c85765023bcfec5e1254ffea228a
Ethel Jerez <ethelmj@gmail.com>
2018-04-10 17:54:49 -0400

    integrated searchapi

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,20 +1,51 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import {SearchkitManager} from 'searchkit'
+import {SearchkitProvider} from 'searchkit'
 import {
-  Form,
-  FormGroup,
-  FormControl,
-  Button
+    SearchBox,
+    RefinementListFilter,
+    Hits,
+    HitsStats,
+    SearchkitComponent,
+    SelectedFilters,
+    MenuFilter,
+    HierarchicalMenuFilter,
+    Pagination,
+    ResetFilters
+    } from "searchkit";
+
+
+
+class SearchForm extends React.Component {
+  render() {
+    const searchkit = new SearchkitManager("http://localhost:5400/search")
+    return (
+      <SearchkitProvider searchkit={searchkit}>
+      </SearchkitProvider>
+    )
+  }
 }
 
-const SearchForm = () => (
-  <Form inline>
-    <FormGroup controlId="formInlineEmail">
-      <FormControl type="search" placeholder="Enter something ..." />
-    </FormGroup>
-    <Button type="submit">
-      search
-    </Button>
-  </Form>
-);
 
+
+// import React from 'react';
+// import {
+//   Form,
+//   FormGroup,
+//   FormControl,
+//   Button
+// }
+//
+// const SearchForm = () => (
+//   <Form inline>
+//     <FormGroup controlId="formInlineEmail">
+//       <FormControl type="search" placeholder="Enter something ..." />
+//     </FormGroup>
+//     <Button type="submit">
+//       search
+//     </Button>
+//   </Form>
+// );
+//
 export default SearchForm;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
 import { setupCognito, cognito } from 'react-cognito';
 import App from './App';
-import SearchForm from '/components/SearchForm'
+import SearchForm from './components/SearchForm'
+import {
+  SearchkitManager, SearchkitProvider, SearchkitComponent, SearchBox, Hits,  RefinementListFilter
+} from "searchkit"
 
 import config from './config.json';
 
+const searchkit = new SearchkitManager("/");
 const reducers = combineReducers({
     cognito,
 });
@@ -17,9 +21,29 @@ let store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.
 // config.group = 'admins'; // Uncomment this to require users to be in a group 'admins'
 setupCognito(store, config);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-    <SearchForm />
-  </Provider>,
-  document.getElementById('root'));
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//     <SearchForm />
+//   </Provider>,
+//   document.getElementById('root'));
+
+class SearchApp extends SearchkitComponent {
+  render() {
+    return (
+      <div>
+        <SearchBox/>
+        <Hits/>
+      </div>
+    )
+  }
+}
+ReactDOM.render((
+    <SearchkitProvider searchkit={searchkit}>
+        <div>
+            <SearchBox/>
+            <RefinementListFilter id="codes" field="codes.raw"/>
+            <Hits/>
+        </div>
+    </SearchkitProvider>
+),  document.getElementById('root'))
