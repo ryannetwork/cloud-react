5934081dead8235d8c83c21985c3c023a456f45d
Valeria Protsan <valeriyapro@gmail.com>
2018-04-17 13:56:38 -0400

    bookmarks for specific user works

--- src/components/bookmarks.js
+++ src/components/bookmarks.js
@@ -2,20 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SmallSearchForm from './SmallSearchForm';
 import axios from 'axios';
+import ResultItem from './ResultItem';
+import '../css/list.css';
 
 
 class Bookmarks extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.state = {
+        data: []
+    }
   }
 
    componentWillMount(){
 
       axios.get(`http://localhost:5400/favorite/${this.props.user.username}`)
-     .then((response) => {
-       for(var key in response){
-         console.log(response[key])
-       }
+     .then((data) => {
+         this.setState({
+             data: data.data
+         })
       })
       .catch(function (error) {
          console.log("error: " + error);
@@ -24,11 +29,17 @@ class Bookmarks extends React.Component {
 
    render() {
 
+     const codeItemEach = this.state.data.map((item, index) =>
+         <li key={index} className="list-item">
+             <ResultItem itemValue={item} key={index}/>
+         </li>
+     );
+
        return (
          <div className="main-code-container bookmarks">
             <h1 className="icd">ICD-10 Medical Coding Reference</h1>
             <SmallSearchForm />
-            {this.props.user.username ? this.props.user.username : "Please log in to be able to create bookmarks"}
+            {this.props.user.username ? <ul className="list">{codeItemEach}</ul> : "Please log in to be able to create bookmarks"}
 
          </div>
        )
